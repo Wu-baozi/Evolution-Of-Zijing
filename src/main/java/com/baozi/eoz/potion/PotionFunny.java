37210a61afb721ed490b1949d6e4395848eb2f92
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2021-01-22 18:20:19 +0800

    为滑稽效果增加图标

--- src/main/java/com/baozi/eoz/potion/PotionFunny.java
+++ src/main/java/com/baozi/eoz/potion/PotionFunny.java
@@ -1,12 +1,29 @@
 package com.baozi.eoz.potion;
 
 import com.baozi.eoz.EOZ;
+import net.minecraft.client.Minecraft;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
 
 public class PotionFunny extends Potion {
+    private static final ResourceLocation TEXTURE = new ResourceLocation(EOZ.MODID+":textures/gui/potion.png");
+
     public PotionFunny(){
         super(false,0xF9C766);
         this.setRegistryName(EOZ.MODID+":"+"funny");
         this.setPotionName("effect."+EOZ.MODID+".funny");
+    }
+
+    @Override
+    public void renderInventoryEffect(int x, int y, PotionEffect e, Minecraft mc){
+        mc.getTextureManager().bindTexture(TEXTURE);
+        mc.currentScreen.drawTexturedModalRect(x+6,y+7,0,0,36,36);
+    }
+
+    @Override
+    public void renderHUDEffect(int x,int y ,PotionEffect e,Minecraft mc,float alpha){
+        mc.getTextureManager().bindTexture(TEXTURE);
+        mc.ingameGUI.drawTexturedModalRect(x+3,y+3,0,0,36,36);
     }
 }
