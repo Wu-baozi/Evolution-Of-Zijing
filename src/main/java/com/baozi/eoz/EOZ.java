4497e3d3fdc4c798abeed72fcabd9726697dc090
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2021-01-22 08:18:40 +0800

    Update EOZ.java

--- src/main/java/com/baozi/eoz/EOZ.java
+++ src/main/java/com/baozi/eoz/EOZ.java
@@ -7,11 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = EOZ.MODID, name = EOZ.NAME, version = EOZ.VERSION)
+public class EOZ
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "eoz";
+    public static final String NAME = "Evolution Of Zijing";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -25,7 +25,6 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
