6258a30abbd774bd389aadf684fadf0cec61e8a1
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2021-01-22 08:24:38 +0800

    增加版本号

--- src/main/java/com/baozi/eoz/EOZ.java
+++ src/main/java/com/baozi/eoz/EOZ.java
@@ -12,7 +12,7 @@ public class EOZ
 {
     public static final String MODID = "eoz";
     public static final String NAME = "Evolution Of Zijing";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "@EOZVERSION@";
 
     private static Logger logger;
 
